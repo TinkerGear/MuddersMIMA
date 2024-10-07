10d9c6d81427b0e2b9615c9344f34c779df3b50e
TinkerGear <147345377+TinkerGear@users.noreply.github.com>
2024-10-06 19:38:16 -0700

    Re-upload

--- muddersMIMA_firmware/config.h
+++ muddersMIMA_firmware/config.h
@@ -8,7 +8,7 @@
 	#include "muddersMIMA.h"  //For Arduino IDE compatibility
 
 	#define FW_VERSION "0.2.0"
-    #define BUILD_DATE "2024JUN05"
+    #define BUILD_DATE "2024OCT06"
 
 	#define CPU_MAP_ATMEGA328p
     
@@ -16,23 +16,44 @@
 
 	#define INVERT_JOYSTICK_DIRECTION //comment to mirror joystick assist and regen directions
 
+  //Adjusts output for sliders that output 20-84% range rather than 5-95%. Added for Balto.
+  //#define SLIDER_IS_INSTALLED
+
+  //Maximum engine RPM before assist is disabled
+  const uint16_t MAX_RPM = 5500; 
+
+  //Configurable delay after clutch depressed before assist re-enabled (in ms)
+  const uint16_t CLUTCH_DELAY = 500; 
+
+  //RPM under which the IMA will derate output to avoid errors
+  const uint16_t DERATE_UNDER_RPM = 2100; 
+
+  //Output percent to derate to if under DERATE_UNDER_RPM
+  const uint8_t DERATE_PERCENT = 70; 
+
+  //Time to ramp from 0 to joystick value (in ms)
+  const uint16_t RAMP_UP_DURATION = 0;
+
 	//choose behavior when three position switch...
 	//...is in the '0' position
 		  #define MODE0_BEHAVIOR() mode_OEM()
 		//#define MODE0_BEHAVIOR() mode_manualAssistRegen_withAutoStartStop();
 		//#define MODE0_BEHAVIOR() mode_manualAssistRegen_ignoreECM();
 		//#define MODE0_BEHAVIOR() mode_INWORK_PHEV_mudder();
+    //#define MODE1_BEHAVIOR() mode_INWORK_PHEV_AfterEffect();
 
 	//...is in the '1' position
 		//#define MODE1_BEHAVIOR() mode_OEM()
-	  	  #define MODE1_BEHAVIOR() mode_manualAssistRegen_withAutoStartStop();
+	  //#define MODE1_BEHAVIOR() mode_manualAssistRegen_withAutoStartStop();
 		//#define MODE1_BEHAVIOR() mode_manualAssistRegen_ignoreECM();
-		//#define MODE1_BEHAVIOR() mode_INWORK_PHEV_mudder();
+		#define MODE1_BEHAVIOR() mode_INWORK_PHEV_mudder();
+    //#define MODE1_BEHAVIOR() mode_INWORK_PHEV_AfterEffect();
 
 	//...is in the '2' position
 		//#define MODE2_BEHAVIOR() mode_OEM()
 		//#define MODE2_BEHAVIOR() mode_manualAssistRegen_withAutoStartStop();
-	  	  #define MODE2_BEHAVIOR() mode_manualAssistRegen_ignoreECM();
+	  //#define MODE2_BEHAVIOR() mode_manualAssistRegen_ignoreECM();
 		//#define MODE2_BEHAVIOR() mode_INWORK_PHEV_mudder();
+    #define MODE2_BEHAVIOR() mode_INWORK_PHEV_AfterEffect();
 
 #endif
